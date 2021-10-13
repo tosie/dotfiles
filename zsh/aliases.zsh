e96f32dcd2765b681b8641fa6edb1a1a60b2b577
Thomas Siebers <tom@tsiebers.de>
2021-10-13 12:49:56 +0200

    alias added: mls (ls with numeric mode output)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
 alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
+# Good 'ol Clear Screen command.
+alias cls='clear'
+
+# `ls` with numeric mode.
+alias mls="ls -hl | awk '{k=0;for(i=0;i<=8;i++)k+=((substr(\$1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(\"%0o \",k);print}'"
