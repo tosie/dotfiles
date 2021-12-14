005600a469fb0fa440eb4c30321eaa7185e1a7b9
Thomas Siebers <tom@tsiebers.de>
2021-12-14 14:22:30 +0100

    Some shellscript improvements and addition of a Brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Homebrew
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+parentDirectory="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+dotfilesDirectory="$(cd "$( dirname "$parentDirectory" )" && pwd -P)"
+
 # Check for Homebrew
-if test ! $(which brew)
+if test ! "$(which brew)"
 then
   echo "  Installing Homebrew for you."
 
@@ -14,11 +17,22 @@ then
   if test "$(uname)" = "Darwin"
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  #
+  # On Linux systems we do not install homebrew. Instead, use the appropriate package manager.
   # elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   # then
   #   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  #
   fi
+fi
 
+if test "$(which brew)"
+then
+  echo "  Using Brewfile to install applications."
+  brew bundle --file "$dotfilesDirectory/homebrew/Brewfile"
+
+  echo "  Upgrading all Homebrew packages."
+  brew upgrade
 fi
 
 exit 0
