c73acd2194b556393300ec788fa6a653fbcba72d
Thomas Siebers <tom@tsiebers.de>
2021-12-14 14:22:30 +0100

    Some shellscript improvements and addition of a Brewfile

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Sets reasonable macOS defaults.
 #
 # Or, in other words, set shit how I like in macOS.
@@ -6,6 +8,11 @@
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
 # Run ./set-defaults.sh and you'll be good to go.
+
+if test ! "$(uname)" = "Darwin"
+  then
+  exit 0
+fi
 
 # Disable press-and-hold for keys in favor of key repeat.
 # defaults write -g ApplePressAndHoldEnabled -bool false
