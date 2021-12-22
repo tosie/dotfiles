d7e50a56947fbc9575f93af1f8b04fb1749afdf4
Thomas Siebers <tom@tsiebers.de>
2021-12-22 13:19:02 +0100

    Changes to EDITOR, VISUAL and aliasing helix on Linux

---
+++ pacman/aliases.zsh
@@ -0,0 +1,4 @@
+# On recent ArchLinux systems, helix is not available via the `hx` command anymore.
+if test "$(uname)" = "Linux"
+  alias hx='helix'
+fi
--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,11 @@
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
   export EDITOR='rmate -w'
-else
+  export VISUAL='rmate -w'
+elif test "$(uname)" = "Linux"; then
+  export EDITOR='hx'
+  export VISUAL='hx'
+elif test "$(uname)" = "Darwin"; then
   export EDITOR='mate -w'
+  export VISUAL='mate -w'
 fi
