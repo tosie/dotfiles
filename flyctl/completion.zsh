203f1812431e54a52d04a8c3ea1be10a7d365ef0
Thomas Siebers <tom@tsiebers.de>
2023-03-21 14:18:59 +0100

    Use brew's completion files instead of delivering our own on

--- flyctl/completion.zsh
+++
@@ -1,9 +0,0 @@
-if type -p brew >/dev/null 2>/dev/null
-then
-  completion="$(brew --prefix)/share/zsh/site-functions/_flyctl"
-
-  if test -f $completion
-  then
-    source $completion
-  fi
-fi
--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,8 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
+# Add each topic folder to fpath so that they can add functions and completion scripts.
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+# Add homebrew's fpath (autocompletions are loaded from there).
+if type -p brew >/dev/null 2>/dev/null
+then
+  fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
+fi
