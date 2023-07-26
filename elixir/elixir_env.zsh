2790c2bbad064aabd0c34c3a49d4112e4095002d
Thomas Siebers <tom@tsiebers.de>
2023-07-26 22:12:08 +0200

    elixir: provide a hint on how to use VSCode as the migrations editor

--- elixir/elixir_env.zsh
+++ elixir/elixir_env.zsh
@@ -1,2 +1,5 @@
 # Enable history in iex.
 export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_file_bytes 1024000 -kernel shell_history_path '\"$HOME/.erlang-history\"'"
+
+# Use VSCode as the default editor for ecto migrations.
+# export ECTO_EDITOR="code -w"
