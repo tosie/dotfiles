b15004c217baff654af95138a9ed77fe4b30f274
Thomas Siebers <tom@tsiebers.de>
2023-09-11 13:39:46 +0200

    add completion support for flyctl's alias 'fly'

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Makes completion work for fly.io's `flyctl` command when just using `fly`.
+compdef _flyctl fly
