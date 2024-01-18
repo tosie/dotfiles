2c549264fa3a10fc1aa347c421495a87dc165dbd
Thomas Siebers <tom@tsiebers.de>
2024-01-18 19:25:30 +0100

    Configured mcfly

--- mcfly/shell-integration.zsh
+++ mcfly/shell-integration.zsh
@@ -1,3 +1,12 @@
+# Documentation: https://github.com/cantino/mcfly
+
 if [[ -o interactive ]]; then
+  # export MCFLY_LIGHT=TRUE
+  # export MCFLY_KEY_SCHEME=vim
+  # export MCFLY_INTERFACE_VIEW=BOTTOM
+  export MCFLY_FUZZY=2
+  export MCFLY_RESULTS=45
+  export MCFLY_PROMPT="❯"
+
   eval "$(mcfly init zsh)"
 fi
