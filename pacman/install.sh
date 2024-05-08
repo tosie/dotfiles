de3e8f87fa69e6f3b45c870f6c9f4754f1c8db16
Thomas Siebers <tom@tsiebers.de>
2024-05-08 17:05:51 +0200

    add `ncdu` and `btop`

--- pacman/install.sh
+++ pacman/install.sh
@@ -12,7 +12,7 @@ then
 fi
 
 echo "  Synchronizing the pacman package databases and installing common packages"
-sudo pacman -Syu --noconfirm --needed pacman-contrib vim sudo mosh tmux wget bash-completion helix mcfly
+sudo pacman -Syu --noconfirm --needed pacman-contrib vim sudo mosh tmux wget bash-completion helix mcfly lnav ncdu btop
 
 # Install rmate.
 if test ! "/usr/local/bin/rmate"
