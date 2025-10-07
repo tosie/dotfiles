ade253160cc8212341b3a27617fa91a83a3e1163
Thomas Siebers <tom@tsiebers.de>
2025-10-07 17:19:07 +0200

    ghostty: initial addition

---
+++ ghostty/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Set up the fastfetch configuration by symlinking to the dotfiles directory.
+
+parentDirectory="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+dotfilesDirectory="$(cd "$( dirname "$parentDirectory" )" && pwd -P)"
+configDirectory="${XDG_CONFIG_HOME:-$HOME/.config}/ghostty"
+
+# Symlink the fastfetch configuration directory to the dotfiles directory.
+if [ ! -d "$configDirectory" ]; then
+  if ln -s "$dotfilesDirectory/ghostty" "$configDirectory"; then
+    echo "  Symlinked the ghostty configuration directory to the dotfiles directory"
+  fi
+fi
+
+exit 0
